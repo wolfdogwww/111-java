fd0400f36f87885d110b4ccd72e76b0cc745c125
worlfdog <73741997+wolfdogwww@users.noreply.github.com>
2022-09-23 11:53:47 +0800

    out

--- 20220923/Test02.java
+++ 20220923/Test02.java
@@ -1,10 +1,6 @@
 public class Test02{
     public static void main (String[] avgs){//輸入
 	System.out.println(avgs[0]); //輸出
-	printStr("Will!");
-    }
-
-    public static void printStr (String str){//輸入
-	System.out.println("My Name = " + str); //輸出
+	System.out.println(avgs[1]);
     }
 }

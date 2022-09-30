33265471352e809b1498959ddea5606924f0766b
worlfdog <73741997+wolfdogwww@users.noreply.github.com>
2022-09-30 08:48:34 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
-    public static void main (String[] avgs){
-	System.out.println("hello world");
+    public static void main (String[] avgs){//輸入
+	System.out.println(avgs[0]); //輸出
+	System.out.println(avgs[1]);
     }
 }

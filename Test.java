c46cba006bbc7f37e855f3eba1c14104704113dc
worlfdog <73741997+wolfdogwww@users.noreply.github.com>
2022-09-30 08:49:59 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 public class Test{
     public static void main (String[] avgs){//輸入
 	System.out.println(avgs[0]); //輸出
-	System.out.println(avgs[1]);
+	printStr("Will!");
+    }
+
+    public static void printStr (String str){//輸入
+	System.out.println("My Name = " + str); //輸出
     }
 }

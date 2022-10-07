8b5044367ce2e9fde98e10a7a006e2651d5bafd0
worlfdog <73741997+wolfdogwww@users.noreply.github.com>
2022-10-07 09:49:48 +0800

    Add files via upload

---
+++ 2022107/Switch3.java
@@ -0,0 +1,37 @@
+//Switch v0.3
+import java.util.Scanner;
+
+public class Switch3{
+    public static void main (String[] avgs){//輸入
+        int grade;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("請輸入你的成績(只能輸入0~100喔!)：");
+        grade = input.nextInt();
+ 	
+	while(grade>100){  //防呆
+	    System.out.println("手有問題嗎?");
+	    System.out.print("再輸入一次：");
+	    grade = input.nextInt();
+         }	
+
+	switch(grade/10){
+	    case 10:
+	    case 9:
+		System.out.println('A');
+		break;
+	    case 8:
+	        System.out.println('B');
+		break;
+	    case 7:
+		System.out.println('C');
+		break;
+	    case 6:
+	        System.out.println('D');
+		break;
+	    default:
+                System.out.println("你考得很爛ㄟ");}
+
+
+}
+}

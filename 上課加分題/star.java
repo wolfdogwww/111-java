60351ffa562ac79a43da06b08473c997a1f36b18
worlfdog <73741997+wolfdogwww@users.noreply.github.com>
2022-10-14 08:50:01 +0800

    Add files via upload
    加分第二題

---
+++ 上課加分題/star.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class star{
+    public static void main (String[] avgs){//輸入
+        int row=0;
+        
+        while(row<5){
+	    int a=0;
+	    while(a<=row){
+	        System.out.print("*");
+	        a++;
+		}
+	    row++;
+	    System.out.println("");
+	    
+	}
+    }
+}
